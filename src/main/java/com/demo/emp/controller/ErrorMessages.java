161cf6210598576103a109cd3ca869dbd7bf747b
unknown <mindc1may283@iiht.tech>
2022-08-01 11:38:00 +0530

    first commit

---
+++ src/main/java/com/demo/emp/controller/ErrorMessages.java
@@ -0,0 +1,20 @@
+package com.demo.emp.controller;
+
+public enum ErrorMessages {
+
+    Employee_already_exists_in_the_database("Employee already exists in the database");
+
+    private  String ErrorMessages;
+
+    ErrorMessages(String errorMessages) {
+        this.ErrorMessages = errorMessages;
+    }
+
+    public String getErrorMessages() {
+        return ErrorMessages;
+    }
+
+    public void setErrorMessages(String errorMessages) {
+        ErrorMessages = errorMessages;
+    }
+}
